20820329bdf2057797932f6e23df42bb3be768b3
MohmmadNada <mohammad.nada_97@yahoo.com>
2021-08-03 13:02:49 +0300

    new repo

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,70 @@
 import Head from 'next/head'
+import { useState } from 'react'
 
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    // add useState as array , first is save place , second fun to set 
+    const [locationMarket,setLocationMarket]=useState([]) // between ()is defult value
+    function cookiesFormHandler(event){
+        event.preventDefault(); // prevent refresh
+        const newLocations={ // new location 
+            locationInput : event.target.location.value,
+            minCustomersInput : event.target.minCustomers.value,
+            maxCustomersInput:event.target.maxCustomers.value,
+            avgCustomersInput:event.target.avgCustomers.value,
+        } // add it to all location
+        setLocationMarket([...locationMarket,newLocations])//get new location data and set add it to collection
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+    }
+    
+    // let dictionary = {};//create new object
+    // dictionary["key1"] = 'value1';//set key1
+    // var key1 = dictionary["key1"];//get key1
+    return (
+    <div className = "">
+        <Head>
+            <title>Cookie-stand-admin</title>
+        </Head>
+        <body className="" >
+            <header className = "relative inset-x-0 top-0 p-2 text-2xl bg-blue-500 h-1/5">
+                <h1>Cookies Stand Admin</h1>
+            </header>
+            <main className = "grid h-screen p-10 bg-gray-500 justify-items-center">
+                <form className='grid w-4/5 grid-flow-col grid-cols-4 grid-rows-4 gap-4 p-3 bg-blue-300 rounded-lg h-3/5' onSubmit={cookiesFormHandler}>
+                   <label className='col-span-4 col-start-1 text-xl text-center '>
+                       Create Cookie Stand
+                   </label>
+                   <label className='col-span-1 col-start-1 text-center' for='location'>
+                       Location
+                   </label>
+                   <input className='col-span-3 col-start-2 px-2 bg-blue-200 rounded-lg' id="location" name="location"/>
+                   <label className="col-span-1 col-start-1 text-center">Minimum Customers per Hour</label>
+                   <input className="px-2 rounded-lg" name="minCustomers"></input>
+                   <label className="col-span-1 col-start-2 text-center">Maximum Customers per Hour</label>
+                   <input className="px-2 rounded-lg" name="maxCustomers"></input>
+                   <label className="col-span-1 col-start-3 text-center">Avarage Customers per Sale</label>
+                   <input className="px-2 rounded-lg" name="avgCustomers"></input>
+                   <button className="col-span-1 col-start-4 row-span-2 bg-blue-400 rounded-lg">Create</button>
+                </form>
+                        <p className='w-4/5 grid-flow-col p-2 text-center bg-blue-300 rounded-lg h-3/5' >
+                        Report table coming soon <br></br>
+                        {
+                            locationMarket.map(item => {
+                                return(
+                                    JSON.stringify(item)
+                                )
+                            })
+                            }
+                        </p>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+                
+            </main>
+            <footer className = "relative inset-x-0 bottom-0 p-2 text-lg bg-blue-500 h-1/5 ">
+                <h5>
+                &copy;2021
+                </h5>
+            </footer> 
+        </body>
+        
     </div>
-  )
+    )
 }
